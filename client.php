b18b74fe3d7a1ff69ee2e6226bc0753109292f91
maxfolder <maxfolder55@gmail.com>
2019-09-03 23:11:29 +0300

    otus-6. Сокеты. Рефакторинг

--- client.php
+++ client.php
@@ -1,32 +1,42 @@
 <?php
+require __DIR__ . '/classes/ClientSocketDataBuilder.php';
+
 header('Content-Type: text/plain;');
 set_time_limit(0);
 ob_implicit_flush();
-$address = 'localhost';
-$port = 8082;
-if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) < 0) {
-    echo "Ошибка создания сокета";
-} else {
-    echo "Сокет создан\n";
+
+$client = (new ClientSocketDataBuilder())
+    ->setHost('localhost')
+    ->setPort(8082)
+    ->setProtocolFamilyForSocket(AF_INET)
+    ->setTypeOfDataExchange(SOCK_STREAM)
+    ->setProtocol(SOL_TCP)
+    ->setMaxByteForRead(1024)
+    ->built();
+
+try {
+    $socket = $client->socketCreate();
+} catch(Throwable $e){
+    echo $e->getMessage();
 }
-$result = socket_connect($socket, $address, $port);
-if ($result === false) {
-    echo "Ошибка при подключении к сокету";
-} else {
-    echo "Подключение к сокету прошло успешно\n";
+
+try {
+    $connection = $client->connect($socket);
+} catch (SocketException $e) {
+    echo $e->getMessage();
 }
 
 do {
     sleep(1);
-    $out = socket_read($socket, 1024);
+    $out = $client->read($socket);
     echo "Сообщение от сервера: $out.\n";
     $msg = 'Принято';
-    socket_write($socket, $msg,  mb_strlen($msg,'cp1251'));
+    $client->write($socket, $msg);
 } while(true);
 
 
 if (isset($socket)) {
-    socket_close($socket);
-    echo "Сокет успешно закрыт";
+    $client->socketClose($socket);
+    echo 'Сокет успешно закрыт';
 }
 
--- server.php
+++ server.php
@@ -1,60 +1,78 @@
 <?php
+
+require __DIR__ . '/classes/ServerSocketDataBuilder.php';
+
 header('Content-Type: text/plain;');
 set_time_limit(0);
 ob_implicit_flush();
-$address = 'localhost';
-$port = 8082;
-if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) < 0) {
-    echo "Ошибка создания сокета";
-} else {
+
+$server = (new ServerSocketDataBuilder())
+    ->setHost('localhost')
+    ->setPort(8082)
+    ->setProtocolFamilyForSocket(AF_INET)
+    ->setTypeOfDataExchange(SOCK_STREAM)
+    ->setProtocol(SOL_TCP)
+    ->setMaxByteForRead(1024)
+    ->built();
+
+
+try {
+    $socket = $server->socketCreate();
     echo "Сокет создан\n";
+} catch (Throwable $e) {
+    echo $e->getMessage();
 }
 
-if (($ret = socket_bind($sock, $address, $port)) < 0) {
-    echo "Ошибка связи сокета с адресом и портом";
-} else {
+try {
+    $res = $server->socketBind($socket);
     echo "Сокет успешно связан с адресом и портом\n";
+} catch (Throwable $e) {
+    echo $e->getMessage();
 }
 
-if (($ret = socket_listen($sock, 5)) < 0) {
-    echo "Ошибка при попытке прослушивания сокета";
-} else {
+
+try {
+    $res = $server->listen($socket);
     echo "Ждём подключение клиента\n";
+} catch (Throwable $e) {
+    echo $e->getMessage();
 }
+
 do {
-    if (($msgsock = socket_accept($sock)) < 0) {
-        echo "Ошибка при старте соединений с сокетом";
-    } else {
-        echo "Сокет готов к приёму сообщений\n";
+    try {
+        $socketConnection = $server->startConnectionWithSocket($socket);
+    } catch (Throwable $e) {
+        echo $e->getMessage();
     }
+
     sleep(2);
     $msg = 'Привет';
-    socket_write($msgsock, $msg,  mb_strlen($msg,'cp1251')); //Запись в сокет
+    $server->write($socketConnection, $msg);
 
     do {
-        if (false === ($buf = socket_read($msgsock, 1024))) {
-            echo "Ошибка при чтении сообщения от клиента";
+        $socketReadResult = $server->read($socketConnection);
+
+        if (!$socketReadResult) {
+            echo 'Ошибка при чтении сообщения от клиента';
         }
 
-        if ($buf === 'exit') {
-            socket_close($msgsock);
+        if ($socketReadResult === 'exit') {
+            $server->socketClose($socketConnection);
             break 2;
         }
-        if (!is_string($buf)) {
-            echo "Сообщение от сервера: передана не строка\n";
-        }
 
-        if ($buf === 'Принято') {
+        if ($socketReadResult === 'Принято') {
             echo sprintf("Сообщение %s принято клиентом\n", $msg);
         }
 
         $msg = mt_rand(1, 10000);
-        socket_write($msgsock, $msg, mb_strlen($msg,'cp1251'));
+
+        $server->write($socketConnection, $msg);
 
     } while (true);
 } while (true);
 
-if (isset($sock)) {
-    socket_close($sock);
-    echo "Сокет успешно закрыт";
+if (isset($socket)) {
+   $server->socketClose($socket);
+    echo 'Сокет успешно закрыт';
 }
