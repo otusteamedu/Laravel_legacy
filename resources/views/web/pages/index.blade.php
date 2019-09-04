bb8f54ff284dd1dbbcbdcac93b5465d0971ede67
DmitryKirillov <esteps.kirillov@gmail.com>
2019-09-04 22:30:27 +0300

    Create front page

--- resources/views/web/pages/index.blade.php
+++ resources/views/web/pages/index.blade.php
@@ -1,5 +1,12 @@
 @extends('web.layouts.main')
+@section('body_class', 'body__front')
 
 @section('content')
-    <h1>Index</h1>
+    <div class="container text-center text-white">
+        <h1 class="pt-5">Strava Clone</h1>
+        <p>Feel free to sign up anytime!</p>
+        <p>
+            <a href="/user/register" class="btn btn-lg btn-primary my-2">Sign up</a>
+        </p>
+    </div>
 @endsection
