f75be35f51d41c3357a840ee37cb9d7e5d1ed102
Egor Gruzdev <egor.gruzdev@gmail.com>
2019-09-05 23:01:03 +0200

    hw9 - секелет проекта, страницы авторизации, регистрации из коробки

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class IndexController
+{
+    public function welcome()
+    {
+        return view('welcome');
+    }
+}
---
+++ app/Routes/WebRoutes.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Routes;
+
+
+use SebastiaanLuca\Router\Routers\Router;
+
+class WebRoutes extends Router
+{
+
+    /**
+     * Map the routes.
+     *
+     * @return void
+     */
+    public function map(): void
+    {
+        $this->router->group(['namespace' => 'App\Http\Controllers', 'middleware' => ['web']], function () {
+           $this->router->auth();
+           $this->router->get('/', 'IndexController@welcome');
+            $this->router->get('/home', 'HomeController@index');
+        });
+    }
+}
