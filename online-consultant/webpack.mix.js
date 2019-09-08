bec05683117acbb9cb9923a2fb3773672d825891
Alexander Pavlyukov <tanzoor@gmail.com>
2019-09-08 14:06:10 +0300

    * make 2 types of resources - app for admin and web for website * setup directory structure for resources * experiment - make simple Vue component * setup Mix and BrowserSync * add new ENV vars for webpack

--- online-consultant/webpack.mix.js
+++ online-consultant/webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.browserSync({
+    proxy: process.env.MIX_BROWSERSYNC_PROXY_URL,
+    https: process.env.MIX_BROWSERSYNC_HTTPS
+});
+
+mix.js('resources/js/app/app.js', 'public/js/app')
+   .js('resources/js/web/web.js', 'public/js/web')
+   .sass('resources/sass/app/app.scss', 'public/css/app')
+   .sass('resources/sass/web/web.scss', 'public/css/web');
