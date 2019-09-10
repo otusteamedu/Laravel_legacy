9f2fd9cccae516e297acebec93ad468bb65ac715
Dmitry Chertok <chertokdmitry@yandex.ru>
2019-09-10 23:16:22 +0300

    laravel 6.0

--- public/config/cache.php
+++ public/config/cache.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -86,9 +88,11 @@ return [
     |
     */
 
+
     'prefix' => env(
         'CACHE_PREFIX',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_cache'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'
     ),
 
 ];
+
