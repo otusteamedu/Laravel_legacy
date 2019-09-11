722ca8515f4c3228deb7af91f25393fa759107bd
vagrant <vagrant@homestead>
2019-09-11 09:06:49 +0000

    changed migration for users

--- blog.loc/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog.loc/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status', 10)->default('unactive');
+            $table->unsignedBigInteger('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
