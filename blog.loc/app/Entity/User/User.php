c5bb1deb39c144af4663c8027d63be5f88e88f9e
vagrant <vagrant@homestead>
2019-09-11 10:44:30 +0000

    added relation between user and role

--- blog.loc/app/Entity/User/User.php
+++ blog.loc/app/Entity/User/User.php
@@ -16,4 +16,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo('App\Entity\User\Role');
+    }
 }
