e9021c44d5982c9944b97a0b1957856fa8a1b68b
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2019-09-13 14:00:47 +0300

    change migration users

--- blog.loc/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog.loc/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('email')->unique();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->date('birthday')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('status', 10)->default('unactive');
