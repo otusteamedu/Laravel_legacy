81774b325e99387c1cef1fd31a400139c0522010
Alexander Pavlyukov <tanzoor@gmail.com>
2019-09-15 00:36:28 +0300

    * add migration, factory and seeder for Company model

---
+++ online-consultant/database/seeds/CompaniesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Company;
+use Illuminate\Database\Seeder;
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Company::class, 5)->create();
+    }
+}
