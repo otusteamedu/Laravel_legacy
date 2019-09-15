36fe44d503b6988380937d1fb5057b1b9cebc9d0
DmitryKirillov <esteps.kirillov@gmail.com>
2019-09-15 15:32:37 +0300

    Add table to index view for Locations

--- resources/views/backend/pages/locations/index.blade.php
+++ resources/views/backend/pages/locations/index.blade.php
@@ -3,7 +3,27 @@
 @section('content')
     <div class="container">
 
-        <p>List</p>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Distance</th>
+                <th scope="col">Created</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($locations as $location)
+                <tr>
+                    <th scope="row">{{ $location->id }}</th>
+                    <th>{{ link_to(route('backend.location.show', ['location' => $location->id]), $location->name) }}</th>
+                    <th>{{ $location->distance }}</th>
+                    <td>{{ $location->created_at }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
         {{ $locations->links() }}
 
     </div>
