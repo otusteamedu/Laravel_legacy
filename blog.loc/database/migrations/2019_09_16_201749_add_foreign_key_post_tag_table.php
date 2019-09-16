64d4a251edfd2d4e51719415129ea45827f7f67e
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2019-09-16 23:21:18 +0300

    added migration for add foreign key post_tag

--- blog.loc/database/migrations/2019_09_16_201749_add_foreign_key_post_tag_table.php
+++ blog.loc/database/migrations/2019_09_16_201749_add_foreign_key_post_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddForeignKeyPostsTable extends Migration
+class AddForeignKeyPostTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class AddForeignKeyPostsTable extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->foreign('user_id')
+        Schema::table('post_tag', function (Blueprint $table) {
+            $table->foreign('post_id')
                 ->references('id')
-                ->on('users');
-            $table->foreign('category_id')
+                ->on('posts');
+            $table->foreign('tag_id')
                 ->references('id')
-                ->on('categories');
+                ->on('tags');
         });
     }
 
@@ -30,9 +30,9 @@ class AddForeignKeyPostsTable extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->dropForeign('posts_user_id_foreign');
-            $table->dropForeign('posts_category_id_foreign');
+        Schema::table('post_tag', function (Blueprint $table) {
+            $table->dropForeign('post_tag_post_id_foreign');
+            $table->dropForeign('post_tag_tag_id_foreign');
         });
     }
 }
