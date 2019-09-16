433798497a5a3367822350cef39a44d3448f590f
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2019-09-16 16:01:50 +0300

    added seeds: Category and Role

---
+++ blog.loc/database/seeds/RolesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')
+            ->insert([
+                'role' => 'Admin',
+                'description' => 'Главная роль',
+            ]);
+    }
+}
