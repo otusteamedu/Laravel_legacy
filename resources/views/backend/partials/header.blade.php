30cf856525c82d5fd921852a705c652183646510
DmitryKirillov <esteps.kirillov@gmail.com>
2019-09-18 16:16:53 +0300

    Add route() to backend header template

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -1,8 +1,9 @@
 <header>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom box-shadow">
-        <h5 class="my-0 mr-md-auto font-weight-normal"><a class="text-white" href="/backend">Backend Strava Clone</a></h5>
+        <h5 class="my-0 mr-md-auto font-weight-normal"><a class="text-white" href="{{ route('backend') }}">Backend Strava Clone</a></h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-white" href="/backend">Home</a>
+            <a class="p-2 text-white" href="{{ route('backend') }}">Backend</a>
+            <a class="p-2 text-white" href="{{ route('backend.location.index') }}">Locations</a>
         </nav>
         <a class="btn btn-outline-primary" href="/">Website</a>
     </div>
