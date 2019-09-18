7e125f4d86b4deb1d028fde72e0b2a6901f33c42
Petr Myazin <petr.myazin@gmail.com>
2019-09-18 17:57:06 +0300

    Fix redirects after register and reset password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/podcasts';
 
     /**
      * Create a new controller instance.
