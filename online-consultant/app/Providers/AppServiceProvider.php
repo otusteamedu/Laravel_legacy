f9c14cda2256ed40b1fba27126c1d455e1882926
Alexander Pavlyukov <tanzoor@gmail.com>
2019-09-23 02:22:24 +0300

    * add interface binding in AppServiceProvider * add soft delete for User * don't delete conversations if lead or widget was deleted

--- online-consultant/app/Providers/AppServiceProvider.php
+++ online-consultant/app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Repositories\Companies\CompanyRepositoryInterface;
+use App\Repositories\Companies\EloquentCompanyRepository;
+use App\Repositories\Conversations\ConversationRepositoryInterface;
+use App\Repositories\Conversations\EloquentConversationRepository;
+use App\Repositories\Leads\EloquentLeadRepository;
+use App\Repositories\Leads\LeadRepositoryInterface;
+use App\Repositories\Users\EloquentUserRepository;
+use App\Repositories\Users\UserRepositoryInterface;
+use App\Repositories\Widgets\EloquentWidgetRepository;
+use App\Repositories\Widgets\WidgetRepositoryInterface;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,17 +22,21 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        //
+        $this->app->bind(CompanyRepositoryInterface::class, EloquentCompanyRepository::class);
+        $this->app->bind(LeadRepositoryInterface::class, EloquentLeadRepository::class);
+        $this->app->bind(WidgetRepositoryInterface::class, EloquentWidgetRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
+        $this->app->bind(ConversationRepositoryInterface::class, EloquentConversationRepository::class);
     }
-
+    
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         Schema::defaultStringLength(191);
     }
