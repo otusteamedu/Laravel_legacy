335c2f0bb8c219b0f9b5a581b7cc7bfc59ef2bc8
Ruslan <ruslan231984@yandex.ru>
2019-11-17 11:41:13 +0300

    Orthography

---
+++ app/Http/Controllers/Admin/OrthographyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Orthography;
+use Illuminate\Http\Request;
+
+class OrthographyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Orthography  $orthography
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Orthography $orthography)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Orthography  $orthography
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Orthography $orthography)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Orthography  $orthography
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Orthography $orthography)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Orthography  $orthography
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Orthography $orthography)
+    {
+        //
+    }
+}
