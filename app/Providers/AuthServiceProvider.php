3d5dd8a446aabc30863d48983aa86117f60fbc0e
Ruslan <ruslan231984@yandex.ru>
2019-11-17 16:24:13 +0300

    policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Grammar;
+use App\Policies\GrammarPolicy;
+use App\Models\User;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+       // 'App\Model' => 'App\Policies\ModelPolicy',
+        Grammar::class => GrammarPolicy::class
     ];
 
     /**
@@ -24,7 +29,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        Gate::define('show-settings', function ($user) {
+            return $user->id == 1;
+        });
+        Gate::define('update-grammar', function ($user,$grammar) {
+            return ($user->id == $grammar->create_user_id||($user->id==1));
+        });
     }
 }
