b4aeb245746e0724e23c4ac1414cbd0a3d4ee90e
Anton Smolko <asmolko@yandex.ru>
2019-11-24 19:03:20 +0300

    Removed unnesessary comments.

--- resource/admin/js/mixins/changingFields.js
+++ resource/admin/js/mixins/changingFields.js
@@ -1,36 +1,3 @@
-export const changeTitle = {
-    methods: {
-        onTitleChange (value) {
-            this.setValidationDelay(this.$v.title);
-            this.$store.dispatch(`${this.store_module}/updateTitleField`, value.trim());
-        }
-    }
-}
-
-export const changeField = {
-    methods: {
-        onFieldChange (name, value, delay = false) {
-            if (this.$v[name] && delay) {
-                this.setValidationDelay(this.$v[name]);
-            } else if (this.$v[name]) {
-                this.$v[name].$touch();
-            }
-
-            const title = name[0].toUpperCase() + name.slice(1);
-            this.$store.dispatch(`${this.store_module}/update${title}Field`, value.trim());
-        }
-    }
-}
-
-export const changeAlias = {
-    methods: {
-        onAliasChange (value) {
-            this.setValidationDelay(this.$v.alias);
-            this.$store.dispatch(`${this.store_module}/updateAliasField`, value.trim());
-        }
-    }
-}
-
 export const changePublishEdit = {
     methods: {
         onPublishChange () {
@@ -44,24 +11,6 @@ export const changePublish = {
     methods: {
         onPublishChange () {
             this.$store.dispatch(`${this.store_module}/updatePublishField`);
-        }
-    }
-}
-
-export const changeDescription = {
-    methods: {
-        onDescriptionChange (value) {
-            this.$v.description.$touch();
-            this.$store.dispatch(`${this.store_module}/updateDescriptionField`, value.trim());
-        }
-    }
-}
-
-export const changeKeywords = {
-    methods: {
-        onKeywordsChange (value) {
-            this.$v.keywords.$touch();
-            this.$store.dispatch(`${this.store_module}/updateKeywordsField`, value.trim());
         }
     }
 }
