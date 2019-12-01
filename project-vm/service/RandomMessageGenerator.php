cb31bce016283ecc9177e4466a91b9f55e701053
andrew_marhai <buk2018irinam@gmail.com>
2019-12-01 22:40:42 +0300

    UNIX сокеты
    В папку project-vm/service добавлены три файла:
     - server.php - для запуска сервера
     - client.php - для запуска клиента
     - RandomMessageGenerator - хелпер для создания сообщений, которые сервер
     отправляет клиенту
    
     Также, папку project-vm/vm-config добавил конфигурационные файлы
     виртуальной машины

---
+++ project-vm/service/RandomMessageGenerator.php
@@ -0,0 +1,43 @@
+<?php
+
+class RandomMessageGenerator
+{
+    private $firstWordsArray = [
+        'Я',
+        'Ты',
+        'Вы',
+        'Он',
+        'Оно',
+        'Кто-то',
+        'Что-то',
+    ];
+    
+    private $secondWordsArray = [
+        'Горбатый',
+        'Лохматый',
+        'Чумазый',
+        'Кривоватый',
+        'Криповатый',
+        'Крутецкий',
+        'Пельмецкий',
+    ];
+    
+    private $thirdWordsArray = [
+        'Чумазод',
+        'Луноход',
+        'Тык-мык-пык',
+        'Размалюн',
+        'Столоед',
+        'Снежеход',
+        'Сын-подруги-мамы',
+    ];
+    
+    public function generateRandomMessage()
+    {
+        $firstWord = $this->firstWordsArray[rand(0, count($this->firstWordsArray) - 1)];
+        $secondWord = $this->secondWordsArray[rand(0, count($this->secondWordsArray) - 1)];
+        $thirdWord = $this->thirdWordsArray[rand(0, count($this->thirdWordsArray) - 1)];
+        
+        return "$firstWord $secondWord $thirdWord";
+    }
+}
