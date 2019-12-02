677e6d572ca3758c7aef83b6ffc71415a7a053eb
Archvile-PC <archvile.bk@ya.ru>
2019-12-02 11:33:59 +0300

    hw3 - работа над ошибками

--- html/app/Application.php
+++ html/app/Application.php
@@ -2,12 +2,19 @@
 
 namespace app;
 
-
+/**
+ * Class Application
+ * @package app
+ */
 class Application
 {
     const SERVER_MODE_OPTION = 'server';
 
-    public function run()
+    /**
+     * Получает параметры из командной строки
+     * @return void
+     */
+    public function run(): void
     {
         $opt = getopt('m:', [self::SERVER_MODE_OPTION]);
         if ((isset($opt['m']) && ($opt['m'] === self::SERVER_MODE_OPTION)) || isset($opt[self::SERVER_MODE_OPTION])) {
--- html/app/Client.php
+++ html/app/Client.php
@@ -1,23 +1,39 @@
 <?php
 
-
 namespace app;
 
+/**
+ * Class Client
+ * @package app
+ */
 class Client extends Socket
 {
 
+    /**
+     * Создает сокет клиента, переводит скрипт в режим ожидания
+     */
     public function __construct()
     {
-        $this->socket = stream_socket_client("unix://" . self::LOCAL_SOCKET, $errno, $errstr);
+        $this->socket = stream_socket_client("unix://" . self::LOCAL_SOCKET);
         if (is_resource($this->socket)) {
             $this->waitMessage();
         }
     }
 
-    public function waitMessage()
+    /**
+     * Ждет сообщение от сервера, отсылает ответ
+     * @return void
+     */
+    public function waitMessage(): void
     {
         while (true) {
-            $msg = trim(fgets($this->socket));
+            $msg = fgets($this->socket);
+
+            if ($msg === false) {
+                break;
+            }
+
+            $msg = trim($msg);
             echo "Сообщение $msg принято клиентом" . PHP_EOL;
             fwrite($this->socket, 'Принято' . PHP_EOL);
         }
