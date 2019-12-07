982afc32b537bf88deb9cc17bddef95e4b19ce61
Archvile-PC <archvile.bk@ya.ru>
2019-12-07 14:04:09 +0300

    laravel init

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
