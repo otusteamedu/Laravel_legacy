72e38edd0b8322d4fbf12ba0f10fa6cd08e9b61c
andrew_marhai <buk2018irinam@gmail.com>
2019-12-09 06:34:21 +0300

    Вывод списка пользователей
    Установил пакет laravel/ui и сгенерировал скаффолды (по-русски звучит не
    очень)) для bootstrap и vue.
    Сгенерировал php artisan ui vue --auth
    Создал группу роутов (пока там только один) для работы с пользователями
    Создал первоначальные варианты blade шаблонов
    Сделал страницу, на которой выводится список всех пользователей

---
+++ resources/lang/en/message.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'navbar' => 'Navbar',
+    'home' => 'Home',
+
+
+
+    'show-user-list-title' => 'The title of my wonderful App',
+    'show-user-list-header' => 'The list of users',
+
+    'show-user-list_table_thead_name' => 'Name',
+    'show-user-list_table_thead_email' => 'Name',
+    'show-user-list_table_thead_created-at' => 'The date of registration',
+
+    'show-user-list-footer-header' => 'Some text in the footer',
+    'show-user-list-footer-text' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil
+                repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
+                harum esse fugiat. Itaque, culpa?',
+];
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="page-footer font-small teal pt-4">
+    <div class="container-fluid text-center text-md-left">
+        <div>
+            <h5 class="text-uppercase font-weight-bold">{{ __('message.show-user-list-footer-header') }}</h5>
+            <p>{{ __('message.show-user-list-footer-text') }}</p>
+
+        </div>
+    </div>
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+        <a href="#"> SuperApp.com</a>
+    </div>
+</footer>
