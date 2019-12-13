7cbe5d419c7709ef3e7f9fa092d19fc81f653cda
Denis Abidov <den-abidov@yandex.ru>
2019-12-13 22:30:28 +0300

    Создал модель UserAccount и её миграцию миграцию _create_user_accounts_table.php

---
+++ database/migrations/2019_12_13_191844_create_user_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('source');
+            $table->string('login')->unique();
+            $table->string('password');
+            $table->integer('cum_sum');
+            $table->double('C');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_accounts');
+    }
+}
