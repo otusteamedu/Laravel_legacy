baec0faeab22fdbdc0cd4881d6593ae94e5df9be
Denis Abidov <den-abidov@yandex.ru>
2019-12-13 22:42:59 +0300

    Создал модель Order и её миграцию миграцию _create_orders_table.php

---
+++ database/migrations/2019_12_13_193100_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->date('date');
+            $table->integer('sum');
+            $table->string('number');
+            $table->string('delivery_type');
+            $table->string('address');
+            $table->string('processed_by');
+            $table->string('comments');
+            $table->string('on_control');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
