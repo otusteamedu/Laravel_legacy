b5cf2b134b2a32ea2ce84cbf85464da443f9aaac
andrew_marhai <buk2018irinam@gmail.com>
2019-12-16 07:36:01 +0300

    Создал миграции

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,18 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->smallInteger('role_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('role_id')
+                ->references('id')->on('roles')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
         });
+
+
     }
 
     /**
---
+++ database/migrations/2019_12_16_041231_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Миграция для категорий товаров и услуг
+ *
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('parent_id')->unsigned()->default(0);
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
