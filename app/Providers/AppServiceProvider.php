25bcd1556ce2588de5f70558f7e821b3ad1afd54
DmitryKirillov <esteps.kirillov@gmail.com>
2019-12-28 21:30:50 +0300

    Register package in Service Provider
    This seems to be the easiest way to get Artisan working with this package in non-CLI environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use DmitryKirillov\ActivityParserLaravel\Console\Commands\ParseTCX;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->commands([
+            ParseTCX::class
+        ]);
     }
 }
