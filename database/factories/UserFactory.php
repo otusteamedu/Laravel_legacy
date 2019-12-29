b802772879b830a3f884c855afe0dc38c195f96e
andrew_marhai <buk2018irinam@gmail.com>
2019-12-30 00:52:53 +0300

    Доработки по фабрике пользователей
    Вместо рандома с захардкоженными минимальным и максимальным id роли,
    сделал подстановку рандомной id роли из реальной БД

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use App\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,14 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $roleId = Role::all()->random()->id;
     return [
         'username' => $faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'role_id' => rand(1, 5),
+        'role_id' => $roleId,
         'description' => $faker->text,
         'icon' => $faker->url
     ];
