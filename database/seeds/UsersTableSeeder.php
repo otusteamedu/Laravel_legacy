5f739790ba6ec7b9caa98d6c0451622bd763af85
andrew_marhai <buk2018irinam@gmail.com>
2020-01-08 01:47:08 +0300

    Добавил в сидер тестового админа

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'username' => 'buk2018irinam',
+            'email' => 'buk2018irinam@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10),
+            'role_id' => 1,
+            'description' => 'Test admin',
+            'icon' => 'https://ya.ru'
+        ]);
         factory(User::class, 50)->create();
     }
 }
