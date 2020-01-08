10b74ecd511cae6b75801e0d1349e2c12198f74b
andrew_marhai <buk2018irinam@gmail.com>
2020-01-08 04:47:01 +0300

    Фронт: Установил и подключил библиотеку для работы с jwt токенами:  @websanova/vue-auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,11 +6,26 @@ import router from "./router";
 import VueBootstrap from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueAuth from '@websanova/vue-auth';
 
 import App from './components/App';
 
+// @websanova/vue-auth
+import websanovaAuth from '@websanova/vue-auth/drivers/auth/bearer';
+import websanovaHttp from '@websanova/vue-auth/drivers/http/axios.1.x';
+import websanovaRouter from '@websanova/vue-auth/drivers/router/vue-router.2.x';
+
+Vue.use(VueAxios, axios);
 Vue.use(VueRouter);
 Vue.use(VueBootstrap);
+
+Vue.use(VueAuth, {
+    auth: websanovaAuth,
+    http: websanovaHttp,
+    router: websanovaRouter
+});
 
 Vue.component('pagination', LaravelPagination);
 
