9c353b8924ef0844751e5f89983a432ff2d8347f
andrew_marhai <buk2018irinam@gmail.com>
2020-01-12 06:23:55 +0300

    Обновление сида для User
    Добавил создание тестовых пользователей для всех ролей

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,13 +13,53 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'ИП Тучкин К.В.',
-            'email' => 'buk2018irinam@gmail.com',
+            'name' => 'Админ тест',
+            'email' => 'admin@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10),
+            'role_id' => 3,
+            'description' => 'Test admin',
+            'icon' => 'https://ya.ru'
+        ]);
+        User::create([
+            'name' => 'Топ менеджер тест',
+            'email' => 'top-manager@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10),
+            'role_id' => 4,
+            'description' => 'Test manager',
+            'icon' => 'https://ya.ru'
+        ]);
+        User::create([
+            'name' => 'Менеджер тест',
+            'email' => 'manager@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10),
+            'role_id' => 5,
+            'description' => 'Test admin',
+            'icon' => 'https://ya.ru'
+        ]);
+        User::create([
+            'name' => 'Частный предприниматель тест',
+            'email' => 'private-entrepreneur@test.com',
             'email_verified_at' => now(),
             'password' => Hash::make('test123'),
             'remember_token' => Str::random(10),
             'role_id' => 1,
-            'description' => 'Test admin',
+            'description' => 'Test manager',
+            'icon' => 'https://ya.ru'
+        ]);
+        User::create([
+            'name' => 'Оптовик тест',
+            'email' => 'wholesaler@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10),
+            'role_id' => 2,
+            'description' => 'Test manager',
             'icon' => 'https://ya.ru'
         ]);
         factory(User::class, 50)->create();
