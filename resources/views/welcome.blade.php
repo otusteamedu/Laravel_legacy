e8c200ebbcfdf09fc8a908404c902bdc3d53d9db
Panov Alexey <alexey.bitrix.spektr@yandex.ru>
2020-02-02 11:58:02 +0300

    Install IDE-helper. Move User model to new namespace: \App\Models.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel. Начало положено :-)
                 </div>
 
                 <div class="links">
