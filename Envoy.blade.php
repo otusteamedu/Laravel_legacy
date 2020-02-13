0a75d83eeba186ba804811a224b0c85cef91ba42
Archvile-PC <archvile.bk@ya.ru>
2020-02-13 12:38:50 +0300

    Deployment

---
+++ Envoy.blade.php
@@ -0,0 +1,32 @@
+@servers(['web' => ['archvile@31.31.192.240']])
+
+@story('deploy')
+    rm
+    git
+    composer
+    init
+    migrate
+@endstory
+
+@task('rm')
+    rm -rf /var/www/archvile/data/www/otus.0x25.ru
+@endtask
+
+@task('git')
+    git clone https://github.com/otusteamedu/Laravel /var/www/archvile/data/www/otus.0x25.ru -b KKondratenko/hw17
+@endtask
+
+@task('composer')
+    cd /var/www/archvile/data/www/otus.0x25.ru/
+    /opt/php73/bin/php /usr/local/bin/composer install
+@endtask
+
+@task('init')
+    cd /var/www/archvile/data/www/otus.0x25.ru/
+    cp .env.example .env
+@endtask
+
+@task('migrate')
+    /opt/php73/bin/php artisan db:wipe
+    /opt/php73/bin/php artisan migrate
+@endtask
