483115851155ac12b290a09dea754d25bbd3d719
Archvile-PC <archvile.bk@ya.ru>
2020-02-15 17:51:05 +0300

    Deployment переделано

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,33 +1,65 @@
 @servers(['web' => ['archvile@31.31.192.240']])
 
+@setup
+    $server = $server ?? 's2';
+    $branch = $branch ?? 'master'
+@endsetup
+
 @story('deploy')
-    rm
-    git
+    toggle_server
+    cache
+    fetch
+    checkout
     composer
-    init
     migrate
+    warm
 @endstory
 
-@task('rm')
-    rm -rf /var/www/archvile/data/www/otus.0x25.ru
+@task('ls')
+    cd ~/www/
+    ls -al
 @endtask
 
-@task('git')
-    git clone https://github.com/otusteamedu/Laravel /var/www/archvile/data/www/otus.0x25.ru -b KKondratenko/hw17
+@task('toggle_server')
+    ln -snf ~/www/{{$server}}.otus.0x25.ru ~/www/otus.0x25.ru
+@endtask
+
+@task('cache')
+    cd ~/www/otus.0x25.ru
+    /opt/php72/bin/php artisan view:clear
+    /opt/php72/bin/php artisan cache:clear
+    /opt/php72/bin/php artisan config:clear
+    /opt/php72/bin/php artisan event:clear
+    /opt/php72/bin/php artisan queue:flush
+    /opt/php72/bin/php artisan route:clear
+@endtask
+
+@task('fetch')
+    cd ~/www/otus.0x25.ru
+    git fetch https://github.com/otusteamedu/Laravel
+@endtask
+
+@task('checkout')
+    cd ~/www/otus.0x25.ru
+    git reset --hard
+    git checkout KKondratenko/{{ $branch }}
 @endtask
 
 @task('composer')
-    cd /var/www/archvile/data/www/otus.0x25.ru/
-    /opt/php73/bin/php /usr/local/bin/composer install
-@endtask
-
-@task('init')
-    cd /var/www/archvile/data/www/otus.0x25.ru/
-    cp .env.example .env
+    cd ~/www/otus.0x25.ru
+    /opt/php72/bin/php /usr/local/bin/composer update --no-scripts
+    /opt/php72/bin/php /usr/local/bin/composer dump-autoload
 @endtask
 
 @task('migrate')
-    cd /var/www/archvile/data/www/otus.0x25.ru/
-    /opt/php73/bin/php artisan db:wipe
-    /opt/php73/bin/php artisan migrate
+    cd ~/www/otus.0x25.ru
+    /opt/php72/bin/php artisan migrate
+@endtask
+
+@task('warm')
+    cd ~/www/otus.0x25.ru
+    /opt/php72/bin/php artisan config:cache
+    /opt/php72/bin/php artisan event:cache
+    /opt/php72/bin/php artisan view:cache
+    {{--/opt/php72/bin/php artisan route:cache--}}
 @endtask
