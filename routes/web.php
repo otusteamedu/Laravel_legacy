a4f5ddbcdab63a4b8096877961d6ee6d383821c9
Boss <ruslangr@yahoo.com>
2020-02-21 21:50:43 +0200

    Init

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::match(['get', 'post'] ,'/home', function () {
+    echo ('welcome');
+});
+
+Route::get('/news/{id}/{name}', function ($id, $name) {
+    return view('welcome');
+});
