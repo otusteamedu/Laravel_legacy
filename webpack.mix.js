e6dab38427bc22f05746a72945cfde59a6698193
Boss <ruslangr@yahoo.com>
2020-02-24 13:24:40 +0200

    auto reload

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,23 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
+
+
+// https://browsersync.io/docs/options
+mix.browserSync({
+    host:   '192.168.56.10',
+    proxy: 'lrtest.loc',
+    open: false,
+    files: [
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'public/js/**/*.js',
+        'public/css/**/*.css'
+    ],
+    watchOptions: {
+        usePolling: true,
+        interval: 500
+    }
+
+});
