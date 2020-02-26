3b303613d5b79aa1b105ee13d61a6a0a000042d9
Anton KC <ajusmith@gmail.com>
2020-02-26 13:50:50 +0300

    models hw

---
+++ app/Models/Country.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Country
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $continent_name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\City[] $cities
+ * @property-read int|null $cities_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereContinentName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereName($value)
+ * @mixin \Eloquent
+ */
+class Country extends Model
+{
+    public $timestamps = false;
+
+    public function city()
+    {
+        return $this->hasMany(City::class);
+    }
+
+    protected $fillable = [
+        'name',
+        'continent_name',
+    ];
+}
---
+++ app/Models/Segment.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Segment
+ *
+ * @property int $id
+ * @property string $name
+ * @property array $condition
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment whereCondition($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Segment whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Segment extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $casts = [
+        'condition' => 'array'
+    ];
+
+    protected $fillable = [
+        'name',
+        'condition',
+    ];
+}
