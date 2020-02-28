9f902b06794fa25c2311fc39cb828dd07506aa18
ivan <slimko2008@yandex.ru>
2020-02-28 19:07:50 +0300

    add test

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -15,32 +15,66 @@ class ProductControllerTest extends TestCase
      *
      * @return void
      */
-    public function testProductIndexExists()
+//    public function testProductIndexExists()
+//    {
+//
+//        $user = factory(User::class)->create([
+//            'name'=>'admintest',
+//            'email'=>'admintest@mail.com',
+//            'password'=>bcrypt($password = 'admin'),
+//            'status'=>'active',
+//            'level'=>1,
+//        ]);
+//
+//
+//        $response = $this->from('/login')->post('/login',[
+//            'email' => $user->email,
+//            'password' => $password,
+//        ]);
+//        $response->assertStatus(302);
+//        $response->assertRedirect('/admin');
+//        $this->assertAuthenticatedAs($user);
+//
+//    }
+
+    public function testProductIndex()
     {
+        Auth::loginUsingId(1);
 
-        $user = factory(User::class)->create([
-            'name'=>'admintest',
-            'email'=>'admintest@mail.com',
-            'password'=>bcrypt($password = 'admin'),
-            'status'=>'active',
-            'level'=>1,
-        ]);
-
-
-        $response = $this->from('/login')->post('/login',[
-            'email' => $user->email,
-            'password' => $password,
-        ]);
-        $response->assertStatus(302);
-        $response->assertRedirect('/admin');
-        $this->assertAuthenticatedAs($user);
-
+        $response = $this->get(route('admin.product.index'));
+        $response->assertStatus(200);
     }
 
     public function testProductCreate()
     {
         Auth::loginUsingId(1);
-        $response = $this->get('/admin/create/');
+
+        $response = $this->get(route('admin.product.create'));
+        $response->assertStatus(200);
+    }
+
+    public function testProductStore()
+    {
+        Auth::loginUsingId(1);
+
+        $response = $this->from(route('admin.product.create'))->post(route('admin.product.store'), [
+            'name' => 'text',
+            'description' => 'text',
+            'price' => 1234,
+            'category_id' => rand(1, 3),
+        ]);
+        $response->assertRedirect(route('admin.product.index'));
+//        $response->assertStatus(200);
+    }
+
+    public function testProductEdit()
+    {
+        Auth::loginUsingId(1);
+
+        $response = $this->from(route('admin.product.index'))->get(route('admin.product.edit',['product'=>1]), [
+            'product' => 1
+        ]);
+//        $response->assertRedirect(route('admin.product.index'));
         $response->assertStatus(200);
     }
 
