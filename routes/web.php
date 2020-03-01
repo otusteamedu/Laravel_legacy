2d32ab040f1035ca0456335ed94211c4409c7b86
smcube <dockerrun@yandex.by>
2020-03-01 14:47:05 +0300

    init

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/page', function () {
+    return view('page');
+});
+
+Route::get('/reg', function () {
+    return view('reg');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
