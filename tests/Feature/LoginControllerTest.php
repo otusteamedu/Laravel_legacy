f76e68d3973eb6bfa5b7974045cc90231561b0be
ivan <slimko2008@yandex.ru>
2020-03-05 19:08:29 +0300

    update test5mart

--- tests/Feature/LoginControllerTest.php
+++ tests/Feature/LoginControllerTest.php
@@ -4,30 +4,49 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * Class LoginControllerTest
+ * @package Tests\Feature
+ * @group LoginTest
+ */
 class LoginControllerTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
+
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
 
     public function testLogin()
     {
-        $user = factory(User::class)->create([
-            'name' => 'admintest',
-            'email' => 'admintest@mail.com',
-            'password' => bcrypt($password = 'admin'),
-            'status' => 'active',
-            'level' => 1,
-        ]);
 
+        $user = $this->usersGenerator('admin');
 
-        $response = $this->from('/login')->post('/login', [
+        //непонятно почему редирект идет на /home
+        //$response = $this->actingAs($user)->from(route('login'))->get(route('login'));
+        //$response->assertRedirect('/admin');
+
+        $response = $this->from(route('login'))->post(route('login'), [
             'email' => $user->email,
-            'password' => $password,
+            'password' => 'admin',
         ]);
-        $response->assertStatus(302);
         $response->assertRedirect('/admin');
+        $response->assertStatus(302);
+
         $this->assertAuthenticatedAs($user);
 
     }
+
+    public static function usersGenerator($state)
+    {
+        return factory(User::class)->state($state)->create();
+    }
+
 }
--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -14,7 +14,8 @@ class ProductControllerTest extends TestCase
 
     public function testProductIndex()
     {
-        Auth::loginUsingId(1);
+        $user = LoginControllerTest::usersGenerator('admin');
+        Auth::login($user);
 
         $response = $this->get(route('admin.product.index'));
         $response->assertStatus(200);
@@ -22,7 +23,8 @@ class ProductControllerTest extends TestCase
 
     public function testProductCreate()
     {
-        Auth::loginUsingId(1);
+        $user = LoginControllerTest::usersGenerator('admin');
+        Auth::login($user);
 
         $response = $this->get(route('admin.product.create'));
         $response->assertStatus(200);
@@ -30,7 +32,8 @@ class ProductControllerTest extends TestCase
 
     public function testProductStore()
     {
-        Auth::loginUsingId(1);
+        $user = LoginControllerTest::usersGenerator('admin');
+        Auth::login($user);
 
         $response = $this->from(route('admin.product.create'))->post(route('admin.product.store'), [
             '_token' => csrf_token(),
@@ -45,31 +48,34 @@ class ProductControllerTest extends TestCase
 
     public function testProductEdit()
     {
-        Auth::loginUsingId(1);
+        $user = LoginControllerTest::usersGenerator('admin');
+        Auth::login($user);
 
         $response = $this->from(route('admin.product.index'))->get(route('admin.product.edit',['product'=>1]));
         $response->assertStatus(200);
     }
 
-//    public function testProductUpdate()
-//    {
-//        Auth::loginUsingId(1);
-//
-//        $response = $this->from(route('admin.product.edit',['product'=>1]))->get(route('admin.product.update',
-//            ['_token' => csrf_token(),
-//            'name' => 'tet2',
-//            'description' => 'tex t text',
-//            'price' => rand(1000, 3000),
-//            'category_id' => rand(1, 3),
-//                ],
-//            Product::class
-//        ));
-//        $response->assertStatus(200);
-//    }
+    public function testProductUpdate()
+    {
+        $user = LoginControllerTest::usersGenerator('admin');
+        Auth::login($user);
+
+        $response = $this->from(route('admin.product.edit',['product'=>1]))->get(route('admin.product.update',
+            ['_token' => csrf_token(),
+            'name' => 'tet2',
+            'description' => 'tex t text',
+            'price' => rand(1000, 3000),
+            'category_id' => rand(1, 3),
+                ],
+            Product::class
+        ));
+        $response->assertStatus(200);
+    }
 
     public function testProductDestroy()
     {
-        Auth::loginUsingId(1);
+        $user = LoginControllerTest::usersGenerator('admin');
+        Auth::login($user);
 
         $response = $this->from(route('admin.product.edit',['product' => 1]))->post(route('admin.product.destroy',
             [
@@ -79,7 +85,6 @@ class ProductControllerTest extends TestCase
             ]
         ));
         $response->assertRedirect(route('admin.product.index'));
-//        dd($response);
     }
 
 }
