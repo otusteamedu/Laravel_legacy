9b642d905023aa7fb9d0133094744769b011c2e5
Evgenij Vesnovskij <evgenij.vesnovskij@mail.ru>
2020-03-14 23:30:29 +0300

    Test

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at->format('d-m-Y'),
+        ];
+    }
+}
