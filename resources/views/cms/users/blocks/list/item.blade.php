707f601c85dfaa988495deedcc9b1efce9170f9e
Anton KC <ajusmith@gmail.com>
2020-04-02 17:25:26 +0300

    change variables in user layout

--- resources/views/cms/users/blocks/list/item.blade.php
+++ resources/views/cms/users/blocks/list/item.blade.php
@@ -1,9 +1,13 @@
-<?php /** @var \App\Models\User $user */ ?>
+<?php
+/** @var \App\Models\User $user */
+/** @var \App\Models\Tariff $segment */
+/** @var \App\Models\Segment $tariff */
+?>
 <tr>
     <th scope="row">{{ link_to(route('cms.users.show', ['user' => $user->id]), $user->id) }}</th>
     <th>{{ link_to(route('cms.users.show', ['user' => $user->id]), $user->name) }}</th>
     <td>{{ link_to(route('cms.users.show', ['user' => $user->id]), $user->email) }}</td>
     <td>{{ link_to(route('cms.users.show', ['user' => $user->id]), $user->phone) }}</td>
-    <td>{{ link_to(route('cms.users.show', ['user' => $user->id]), $country = App\Models\Tariff::find(1)->name) }}</td>
-    <td>{{ link_to(route('cms.users.show', ['user' => $user->id]), $country = App\Models\Segment::find(1)->name) }}</td>
+    <td>{{ link_to(route('cms.users.show', ['user' => $user->id]), $tariff = App\Models\Tariff::find(1)->name) }}</td>
+    <td>{{ link_to(route('cms.users.show', ['user' => $user->id]), $segment = App\Models\Segment::find(1)->name) }}</td>
 </tr>
