66ccb287cc818c64654764c616d3987ebac6b077
Panov Alexey <alexey.bitrix.spektr@yandex.ru>
2020-04-12 17:45:25 +0300

    Add css and js for events section.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+require('./event');
 
 window.Vue = require('vue');
 
@@ -19,7 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +28,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
+/*const app = new Vue({
     el: '#app',
-});
+});*/
