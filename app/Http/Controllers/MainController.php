9a1b89dbee0c9e701eaba23180b2dadece826eb9
RomIII <1633131@mail.ru>
2020-05-03 11:36:39 +0300

    hw12 done

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+use App\Models\Reason;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class MainController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
@@ -13,8 +18,32 @@ class MainController extends Controller
      */
     public function index()
     {
-        return view('layouts.page_main', [
+        $students = Student::all();
+        $reasons = Reason::all();
 
+        $reasonsCount = count($reasons);
+
+
+        $key = 'array_transaction';
+
+        //было 110 запросов(778ms), стало 2 (264ms)
+        $arTransactions = Cache::remember($key, 60, function () use ($students, $reasons) {
+            foreach ($students as $student) {
+                foreach ($reasons as $reason) {
+                    $arTransactions[$student['id']][$reason['id']] = Transaction::where('student_id', $student['id'])->where('reason_id', $reason['id'])->pluck('amount')->first();
+                }
+            }
+            return $arTransactions;
+        });
+
+
+//        dd($reasons);
+
+        return view('layouts.page_main', [
+            'students' => $students,
+            'reasons' => $reasons,
+            'reasonsCount' => $reasonsCount,
+            'arTransactions' => $arTransactions
         ]);
     }
 }
