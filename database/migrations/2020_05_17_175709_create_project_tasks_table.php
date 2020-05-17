3a7c70737abfd7d3a2861dea1ee5e0cc22c646f3
Alexander Drek <drek2005@gmail.com>
2020-05-17 22:14:19 +0300

    HW2: Структура проекта (миграции + seed's)

---
+++ database/migrations/2020_05_17_175709_create_project_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->longText('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_tasks', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['project_id']);
+        });
+
+        Schema::dropIfExists('project_tasks');
+    }
+}
