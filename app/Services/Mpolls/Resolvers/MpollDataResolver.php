eeda9e74406e49ae1398ee18addfb4795fe3af11
Boss <ruslangr@yahoo.com>
2020-05-22 20:02:55 +0300

    Temp

--- app/Services/Mpolls/Resolvers/MpollDataResolver.php
+++ app/Services/Mpolls/Resolvers/MpollDataResolver.php
@@ -12,7 +12,6 @@ class MpollDataResolver
     public function resolve($data)
     {
         $data['price'] = 5.5;
-//        $data['vvvvv'] = 5.5;
         $data['created_user_id'] = Auth::user()->id;
         return $data;
     }
