21158620cee7092910a00ad0f3348096b5618181
webdev2030 <ya@vyacheslav.kz>
2020-06-11 13:32:24 +0600

    Gitlab-ci

---
+++ Envoy.blade.php
@@ -0,0 +1,10 @@
+@servers(['web' => ['laradock@95.214.9.235']])
+
+@task('foo', ['on' => 'web'])
+    cd /home/laradock/laradock
+    docker-compose exec -T -u laradock workspace bash -c "cd /var/www/instagraphia.kz && \
+npm i && \
+npm audit fix && \
+composer install && \
+php artisan migrate"
+@endtask
