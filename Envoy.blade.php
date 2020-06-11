dacabe3c197d2bbd75aca7d9d849c7598bae66a1
webdev2030 <ya@vyacheslav.kz>
2020-06-11 13:37:25 +0600

    Gitlab-ci

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,8 @@
 @servers(['web' => ['laradock@95.214.9.235']])
 
 @task('foo', ['on' => 'web'])
+    cd /home/laradock/htdocs/instagraphia.kz
+    git pull origin master
     cd /home/laradock/laradock
     docker-compose exec -T -u laradock workspace bash -c "cd /var/www/instagraphia.kz && \
 npm i && \
