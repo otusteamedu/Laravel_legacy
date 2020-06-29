40a7695d924fcc4429338c8ab4d9b6e57c97c8b1
Andy <avp365@yandex.ru>
2020-06-29 17:30:50 +0300

    Домашнее задании 9. Тестируем приложение.

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single','slack'],
             'ignore_exceptions' => false,
         ],
 
