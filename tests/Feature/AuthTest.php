befd6d7a78185e60e2edad8d4ba0586ecdc531da
Dmitrii Shitikov <vagrant@homestead>
2020-07-08 15:02:01 +0000

    auth test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use Tests\TestCase;
+
+/**
+ * Class AuthTest
+ * @package Tests\Feature
+ * @group auth
+ */
+class AuthTest extends TestCase
+{
+    /**
+     * @var User
+     */
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(\RoleSeeder::class);
+
+        $this->user = factory(User::class)->create([
+            'role_id' => Role::METHODIST,
+        ]);
+    }
+
+    /**
+     * POST /login
+     */
+    public function testLogin(): void
+    {
+        /**
+         * FAIL
+         */
+        $body = [
+            'email' => $this->user->email,
+            'password' => 'fail',
+        ];
+        $this->post(route('login'), $body)
+            ->assertRedirect('/');
+
+        /**
+         * Success
+         */
+        $body = [
+            'email' => $this->user->email,
+            'password' => 'password',
+        ];
+        $this->post(route('login'), $body)
+            ->assertRedirect(route('dashboard'));
+    }
+
+    /**
+     * POST /logout
+     */
+    public function testLogout():void
+    {
+        $this->post(route('logout'))
+            ->assertRedirect('/');
+    }
+}
