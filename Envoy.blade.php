c04249b2371d18e3ef7d69be816b2355aafa0613
vetalm1 <vetalf.m3000@gmail.com>
2020-07-17 01:40:40 +1000

    HW16 DDD (rework)

---
+++ Envoy.blade.php
@@ -0,0 +1,32 @@
+
+
+@servers(['web' => ['user@80.78.254.239']])
+
+@story('init')
+    git_init
+    composer
+@endstory
+
+@story('deploy')
+    git_clone
+    composer
+@endstory
+
+@task('git_init', ['on' => 'web'])
+    git clone --single-branch --branch VMeshavkin/hw16 https://github.com/otusteamedu/Laravel.git
+@endtask
+
+@task('git_clone', ['on' => 'web'])
+    cd Laravel
+    git pull origin VMeshavkin/hw16
+@endtask
+
+@task('composer', ['on' => 'web'])
+    cd Laravel
+    composer install
+@endtask
+
+
+@task('test', ['on' => 'web'])
+    php vendor/bin/phpunit
+@endtask
