66eacc3e9f5374b0ec8fd43c01cf71e05fa86265
Ozy <Ozy@nxt.ru>
2020-07-31 14:33:57 +0300

    Добавлен новый сервис для регистрации Gate

---
+++ src/app/Providers/GateServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
